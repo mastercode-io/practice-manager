62c822f498c5eeaeb85b38df44ed4318bfe60b73
Alex Sherin <al@mastercode.io>
2023-08-11 11:42:02 +0100

    Alex remote commit

--- client_code/Forms/CaseWorkflowForm.py
+++ client_code/Forms/CaseWorkflowForm.py
@@ -12,7 +12,7 @@ class CaseWorkflowForm(FormBase):
                                          on_change=self.update_workflow_name)
         self.items = LookupInput(name='items', label='Items', model='CaseWorkflowItem', select='multi', 
                                  text_field='item_name', add_item_text='Add Item', 
-                                 add_item_model='CaseWorkflowItem', add_item_form=Forms.CaseWorkflowItemForm)
+                                 add_item_form=Forms.CaseWorkflowItemForm)
         
         fields = [self.name, self.practice_area, self.items]
         
@@ -21,4 +21,4 @@ class CaseWorkflowForm(FormBase):
         
     def update_workflow_name(self, args):
         print('update_worflow_name')
-        self.name.value = self.practice_area.value
+        self.name.value = self.practice_area.value['name']
